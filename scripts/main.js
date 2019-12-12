f49e94739b72ad69ccc2dad083035523bacf54ab
wggreen <wggreen.nss@gmail.com>
2019-12-12 11:54:12 -0600

    dynamically rendered form

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 import {getEntries} from "./JournalDataProvider.js"
 import {EntryListComponent} from './JournalEntryList.js'
-import {renderForm} from "./JournalForm.js"
+import {JournalFormComponent} from "./JournalForm.js"
 import {EntryFormComponent} from "./JournalEntryComponent.js"
 
-renderForm()
+JournalFormComponent()
 getEntries()
     .then(() => EntryFormComponent())
     .then(() => EntryListComponent())
